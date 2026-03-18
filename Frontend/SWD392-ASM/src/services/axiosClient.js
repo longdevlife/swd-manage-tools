f59767ecb3be49622929883cf599fdb184fdd26b
Khoalh <163608473+khoalhdeveloper@users.noreply.github.com>
2026-03-18 23:18:18 +0700

    add timeout api

--- Frontend/SWD392-ASM/src/services/axiosClient.js
+++ Frontend/SWD392-ASM/src/services/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
-  timeout: 10000,
+  timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
   },
